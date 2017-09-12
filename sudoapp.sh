6fd9ce80f25425f64f30e26870487e5c6b533f34
NotAlexNoyle <alexnoyle@icloud.com>
2017-09-11 21:12:55 -0400

    Fix initial build

--- sudoapp.sh
+++ sudoapp.sh
@@ -1,7 +1,8 @@
 project=$(grep APPLICATION_NAME Makefile | awk '{print $3}')
 echo "creating root-permission script for: $project"
 
-target=_/DEBIAN/postinst
+mkdir DEBIAN && cd DEBIAN && touch postinst && cd ..
+target=DEBIAN/postinst
 cp sudoapp.template $target
 perl -p -i -e "s,<ProjectName>,$project,g" $target
 chmod 0555 $target
